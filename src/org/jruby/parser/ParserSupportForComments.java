3f6bc1cd416ee74872b0ebee647cb1ef71540fe6
Thomas Enebo <tom.enebo@gmail.com>
2006-10-06 20:31:28 +0000

    Simplification of comment parser support
    git-svn-id: http://svn.codehaus.org/jruby/branches/refactoring@2307 961051c9-f516-0410-bf72-c9f7e237a7b7

--- src/org/jruby/parser/ParserSupportForComments.java
+++ src/org/jruby/parser/ParserSupportForComments.java
@@ -1,73 +1,39 @@
 package org.jruby.parser;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.ListIterator;
-
 import org.jruby.ast.ListNode;
 import org.jruby.ast.Node;
 import org.jruby.lexer.yacc.ICommentable;
 
 public class ParserSupportForComments extends ParserSupport {
-	
-	public ParserSupportForComments() {
-		super();
-	}
-
-	public Node introduceComment(Node node, Object[] yaccValues){
-		
+	public Node introduceComment(Node node, Object[] values) {
+        // Enebo: Bummer that this is true
         if (node == null) {
             return null;
         }
-        
-		ArrayList yaccValueList = new ArrayList(Arrays.asList(yaccValues));
-		Iterator valueItr = yaccValueList.iterator();
+
+        for (int i = 0; i < values.length; i++) {
+            if (!(values[i] instanceof ICommentable)) {
+                continue;
+            }
+            
+            ICommentable commentable = (ICommentable) values[i];
+            
+            if (commentable.hasComments()) {
+                node.addComments(commentable.getComments());
+            }
+        }
 		
-		while(valueItr.hasNext()){
-			
-			Object currentValue = valueItr.next();
-			if((currentValue instanceof ICommentable) && ((ICommentable)currentValue).hasComments()){
-				continue;
-			}else{
-				valueItr.remove();
-			}
-		}
-		
-		if(yaccValueList.isEmpty()){
-			return node;
-		}else{
-			Iterator commentItr = yaccValueList.iterator();
-			
-			while(commentItr.hasNext()){
-				ICommentable currentCommentable = (ICommentable)commentItr.next();
-				if(currentCommentable.hasComments()){
-					node.addComments(currentCommentable.getComments());
-				}			
-			}
-			return node;
-		}
+        return node;
 	}
 	
 	public ListNode commentLastElement(ListNode node, Object[] yaccValues) {
-		
-		ListIterator revItr = node.reverseIterator();
-		Node commentedNode = null;
-		
-		if(revItr.hasPrevious())
-		{
-			commentedNode = (Node)revItr.previous();
-			revItr.remove();
-		}
-		else{
-			return node;
-		}
-		
-		commentedNode = introduceComment(commentedNode, yaccValues);
-		
-		node.add(commentedNode);
+        Node commentedNode = node.getLast();
+        
+        // Enebo: If no children nodes where should the comments go?
+        if (commentedNode != null) {
+            introduceComment(commentedNode, yaccValues);
+        }
 		
 		return node;
 	}
-	
 }
