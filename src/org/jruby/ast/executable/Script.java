845a5a63af1896dda0e50a4e2547cd795456e283
Charles Oliver Nutter <headius@headius.com>
2007-01-05 05:24:02 +0000

    creating new headius branch
    git-svn-id: http://svn.codehaus.org/jruby/branches/headius@2672 961051c9-f516-0410-bf72-c9f7e237a7b7

--- src/org/jruby/ast/executable/Script.java
+++
@@ -1,8 +0,0 @@
-package org.jruby.ast.executable;
-
-import org.jruby.runtime.ThreadContext;
-import org.jruby.runtime.builtin.IRubyObject;
-
-public interface Script {
-    public IRubyObject run(ThreadContext context, IRubyObject self);
-}
