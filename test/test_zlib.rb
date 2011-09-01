22ebc32d4c92f11d79db0d760278ac8bbaf1c3fd
Atsuhiko Yamanaka <atsuhiko.yamanaka@gmail.com>
2011-09-02 08:29:49 +0900

    fix miss-usage of Inflate#set_dictionary

--- test/test_zlib.rb
+++ test/test_zlib.rb
@@ -703,10 +703,10 @@ class TestZlibInflateAuto < Test::Unit::TestCase
       i.inflate(comp_str)
       rescue Zlib::NeedDict
     end
-    i.reset
+    #i.reset
 
     i.set_dictionary(dict)
-    i << comp_str
+    i << ""
     assert_equal(str, i.finish)
   end
 end
